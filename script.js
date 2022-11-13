21921474267df589f9d06670d75771a84bafdb65
Peter Ahn <peterahn8@gmail.com>
2022-11-13 08:20:58 -0500

    Refactor code

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
 
+const button = document.querySelectorAll("button");
+
+rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
+paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
+scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
+
 function getComputerChoice() {
     const randomNum = Math.random();
     if (randomNum < (1/3)) {
@@ -16,56 +22,65 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else if (computerSelection === "paper") {
-            computerScore = ++computerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else {
-            playerScore = ++playerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            playerScore = ++playerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else if (computerSelection === "paper") {
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else {
-            computerScore = ++computerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            computerScore = ++computerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else if (computerSelection === "paper") {
-            playerScore = ++playerScore;
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        } else {
-            const resultHeader = document.createElement("h1");
-            resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
-            resultDiv.appendChild(resultHeader);
-        }
+    if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        playerScore = playerScore++;
+    } else {
+        computerScore = computerScore++;
     }
 }
+
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = playerSelection.toLowerCase();
+//     if (playerSelection === "rock") {
+//         if (computerSelection === "rock") {
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else if (computerSelection === "paper") {
+//             computerScore = ++computerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else {
+//             playerScore = ++playerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         }
+//     } else if (playerSelection === "paper") {
+//         if (computerSelection === "rock") {
+//             playerScore = ++playerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else if (computerSelection === "paper") {
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else {
+//             computerScore = ++computerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         }
+//     } else if (playerSelection === "scissors") {
+//         if (computerSelection === "rock") {
+//             computerScore = ++computerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else if (computerSelection === "paper") {
+//             playerScore = ++playerScore;
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         } else {
+//             const resultHeader = document.createElement("h1");
+//             resultHeader.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}. The score is now ${playerScore} to ${computerScore}.`;
+//             resultDiv.appendChild(resultHeader);
+//         }
+//     }
+// }
 
 // function game() {
 //     for (let counter = 0; counter < 5; counter++) {
@@ -87,22 +102,3 @@ function playRound(playerSelection, computerSelection) {
 //     computerScore = 0;
 // }
 
-const buttonsDiv = document.createElement("div");
-const resultDiv = document.createElement("div");
-const rockBtn = document.createElement("button");
-const paperBtn = document.createElement("button");
-const scissorsBtn = document.createElement("button");
-
-document.body.appendChild(buttonsDiv);
-document.body.appendChild(resultDiv);
-buttonsDiv.appendChild(rockBtn);
-buttonsDiv.appendChild(paperBtn);
-buttonsDiv.appendChild(scissorsBtn);
-
-rockBtn.textContent = "Rock";
-paperBtn.textContent = "Paper";
-scissorsBtn.textContent = "Scissors";
-
-rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
-paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
-scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
